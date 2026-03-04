7533854e52d024f1ccc47948f7fab21516ef7996
xiejiayu <xiejiay8017@fxiaoke.com>
2026-03-04 20:49:04 +0800

    ✨ feat(JacksonConfig): 配置 Jackson 全局忽略未知属性

--- sharecrm-im-gateway/src/main/java/com/fxiaoke/sharecrm/im/gateway/config/JacksonConfig.java
+++ sharecrm-im-gateway/src/main/java/com/fxiaoke/sharecrm/im/gateway/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.fxiaoke.sharecrm.im.gateway.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,9 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 全局忽略未知属性
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
     }
 }
