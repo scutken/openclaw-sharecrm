a6434febb566d04ca699f385355552afd88ce3f6
xiejiayu <xiejiay8017@fxiaoke.com>
2026-03-05 11:25:51 +0800

    🗑️ chore(accounts.yml): 删除硬编码的账号配置文件 📦 build(pom.xml): 将打包方式从jar改为war ✨ feat(ServletInitializer): 添加ServletInitializer以支持war部署

---
+++ sharecrm-im-gateway/src/main/java/com/fxiaoke/sharecrm/im/gateway/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fxiaoke.sharecrm.im.gateway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ImGatewayApplication.class);
+    }
+
+}
